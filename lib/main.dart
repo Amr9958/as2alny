9ef173a82cb2998c2ab269c889996a5298971ca2
ahmed abdelsalamm <aabdelsalam854@gmail.com>
2024-06-09 18:41:01 +0300

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
+import 'package:as2lny_app/features/home/presentation/view/home_view.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:google_generative_ai/google_generative_ai.dart';
-import 'dart:io';
 
 void main() {
-  runApp(ChatGPTApp());
+  runApp(const ChatGPTApp());
 }
 
 class ChatGPTApp extends StatelessWidget {
+  const ChatGPTApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,99 +16,7 @@ class ChatGPTApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChatScreen(),
-    );
-  }
-}
-
-class ChatScreen extends StatefulWidget {
-  @override
-  _ChatScreenState createState() => _ChatScreenState();
-}
-
-class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _controller = TextEditingController();
-  final List<String> _messages = [];
-  bool _isLoading = false;
-
-  Future<void> _sendMessage(String message) async {
-    if (message.isEmpty) return;
-
-    setState(() {
-      _messages.add("You: $message");
-      _isLoading = true;
-    });
-
-    final response = await _getGiminiResponse(message);
-
-    setState(() {
-      _messages.add("Bot: ${response ?? 'Error'}");
-      _isLoading = false;
-
-      
-    });
-  }
-
-  Future<String?> _getGiminiResponse(String message) async {
-    final apiKey =
-        Platform.environment['AIzaSyD0TDsH61VwRdjNGkfZ4BAOLYIZ72n7-lY{\rtf1}'];
-    final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey!);
-    final content = [Content.text(_controller.text)];
-
-    try {
-      final response = await model.generateContent(content);
-      print('the response is '+'${response.text}');
-      return response.text;
-    } catch (e) {
-      print('Error: $e');
-    }
-    return null;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('As2alny App'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_messages[index]),
-                );
-              },
-            ),
-          ),
-          if (_isLoading) const CircularProgressIndicator(),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      labelText: 'Enter your message',
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () {
-                    final message = _controller.text;
-                    _controller.clear();
-                    _sendMessage(message);
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      home: const ChatScreen(),
     );
   }
 }
